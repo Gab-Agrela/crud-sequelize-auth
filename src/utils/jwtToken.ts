7fc28ffaeef180ccd7342458b86736f6954934db
Agrela <gabriel.agrela@hotmail.com>
2024-03-05 19:36:09 -0300

    feat: add token in login response

---
+++ src/utils/jwtToken.ts
@@ -0,0 +1,19 @@
+import jose from "jose";
+import "dotenv/config";
+
+const generateJwtToken = async (username: string, id: number) => {
+  const { SECRET_JWT, ALG_JWT, ISSUER_JWT, AUDIENCE_JWT } = process.env;
+  const secretKey = new TextEncoder().encode(SECRET_JWT);
+
+  const token = await new jose.SignJWT({ username, id })
+    .setProtectedHeader({ alg: ALG_JWT as string })
+    .setIssuedAt()
+    .setIssuer(ISSUER_JWT as string)
+    .setAudience(AUDIENCE_JWT as string)
+    .setExpirationTime("10h")
+    .sign(secretKey);
+
+  return token;
+};
+
+export default generateJwtToken;
