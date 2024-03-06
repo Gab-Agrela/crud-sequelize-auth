ef6479110d5dac52ca364ae129aecd8eb805a5e4
GabrielAgrela <gabriel.agrela@hotmail.com>
2024-03-06 08:32:29 -0300

    fix: update SignJwt import

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -1,11 +1,11 @@
-import jose from "jose";
+import { SignJWT } from "jose";
 import "dotenv/config";
 
 const generateJwtToken = async (username: string, id: number) => {
   const { SECRET_JWT, ALG_JWT, ISSUER_JWT, AUDIENCE_JWT } = process.env;
   const secretKey = new TextEncoder().encode(SECRET_JWT);
 
-  const token = await new jose.SignJWT({ username, id })
+  const token = await new SignJWT({ username, id })
     .setProtectedHeader({ alg: ALG_JWT as string })
     .setIssuedAt()
     .setIssuer(ISSUER_JWT as string)
