831481762cd5f745819d14de08407c1117be4b18
Agrela <gabriel.agrela@hotmail.com>
2024-03-07 20:44:34 -0300

    fix: remove commented app use

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(router);
+app.use(router);
 
-// app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-//   return res.status(500).json({ message: err.message });
-// });
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  return res.status(500).json({ message: err.message });
+});
 
 const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => res.status(200).json({ message: "aaaa" }));
