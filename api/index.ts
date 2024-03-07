8e1c3a05e2ba8dda554c0e36a8a7b84b66e29181
Agrela <gabriel.agrela@hotmail.com>
2024-03-07 18:29:56 -0300

    aa

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,17 @@
-import app from "../src/app";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import router from "../src/routes/index";
 import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  return res.status(500).json({ message: err.message });
+});
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,3 +20,5 @@ app.get("/", (_req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+export default app;
