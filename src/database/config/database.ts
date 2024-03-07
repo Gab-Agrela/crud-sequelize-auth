b9d52f9c7257376811dd5f6bccf6ee49bc2c9bb1
Agrela <gabriel.agrela@hotmail.com>
2024-03-07 19:11:48 -0300

    1

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,9 +1,6 @@
-import * as pg from "pg";
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
 const { POSTGRES_URL } = process.env;
 
-export const db = new Sequelize(POSTGRES_URL, {
-  dialectModule: pg,
-});
+export const db = new Sequelize(POSTGRES_URL);
